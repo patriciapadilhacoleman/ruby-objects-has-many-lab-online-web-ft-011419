7432b6b81724b80d1d5f8ee4bf3691f5410c1b08
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-31 17:25:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,21 @@
 class Post
   
   attr_accessor :title, :author_name
+  @@all = []
   
   def initialize(title)
     
     @title = title
     @author_name = author
-    
+    @@all << self
     
   end
+  
+  def self.all
+    
+    @@all
+    
+  end
+
     
 end
